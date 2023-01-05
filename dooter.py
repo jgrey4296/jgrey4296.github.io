2b53f4cfd445560fc38017dac00882fb15c53580
John Grey <johngrey4296@gmail.com>
2023-01-05 18:43:16 +0000

    [chore]
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

---
+++ dooter.py
@@ -0,0 +1,32 @@
+# -*- mode:doit; -*-
+"""
+Stub dooter file for task authoring
+
+"""
+# https://pydoit.org/
+##-- imports
+import pathlib as pl
+from doit.action import CmdAction
+from doit import create_after
+from doit.tools import set_trace, Interactive, PythonInteractiveAction
+from doit.task import clean_targets
+
+import doot
+##-- end imports
+
+##-- config
+DOIT_CONFIG = {
+    "action_string_formatting" : "new"
+}
+
+##-- end config
+
+##-- post-config doot imports
+from doot.groups import *
+from doot.groups_secondary import *
+##-- end post-config doot imports
+
+
+if __name__ == "dooter":
+    # the equivalent of main
+    pass
