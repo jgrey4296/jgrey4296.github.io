2f7adf0f432b79e7160e2cc9da43227b8e4bbeb1
John Grey <johngrey4296@gmail.com>
2023-01-17 00:21:53 +0000

    [chore]
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- dooter.py
+++ dooter.py
@@ -5,7 +5,10 @@ Stub dooter file for task authoring
 """
 # https://pydoit.org/
 ##-- imports
+from __future__ import annotations
 import pathlib as pl
+import sys
+import shutil
 from doit.action import CmdAction
 from doit import create_after
 from doit.tools import set_trace, Interactive, PythonInteractiveAction
@@ -22,11 +25,15 @@ DOIT_CONFIG = {
 ##-- end config
 
 ##-- post-config doot imports
-from doot.groups import *
-from doot.groups_secondary import *
+# from doot.taskslib.groups import *
+# from doot.taskslib.groups_secondary import *
 ##-- end post-config doot imports
 
+from bkmkorg.doot_tasks import bibtex
 
 if __name__ == "dooter":
     # the equivalent of main
+    bib_locs = doot.locs.extend(name="bibtex", src="resources/bibliography")
+    cleaner = bibtex.BibtexClean(dirs=bib_locs)
+    report = bibtex.BibtexReport(dirs=bib_locs)
     pass
