bb3a1f2004969a1f6db66c9da29f537914520cce
John Grey <johngrey4296@gmail.com>
2024-11-01 00:27:23 +0000

    [refactor]: pelican config

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+## pelicanconf.py -*- mode: Py -*-
+
+AUTHOR       = 'John Grey'
+SITENAME     = 'Mostly Harmless'
+SITEURL      = "https://jgrey4296.github.io"
+
+PATH         = "."
+
+TIMEZONE     = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM         = None
+CATEGORY_FEED_ATOM    = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM      = None
+AUTHOR_FEED_RSS       = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
